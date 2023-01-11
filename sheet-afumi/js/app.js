41e2813496f806696921340de6414af8f4d7c088
Angel Morinigo <angelmorinigo2020@gmail.com>
2023-01-11 13:23:47 -0300

    copiado de ejmplo

--- sheet-afumi/js/app.js
+++ sheet-afumi/js/app.js
@@ -1,18 +1,32 @@
 const formulario =  document.getElementById('formulario')
+const data = {
+    Nombres: "angel",
+    Telefono: "0975624601",
+    Estado: "1",
+    "fecha": new Date(),
+  };
 
 formulario.addEventListener('submit',(e)=>{
     e.preventDefault();
  // Add one line to the sheet
-fetch("https://sheet.best/api/sheets/13b8502d-4902-40aa-8fda-a4667229ee9f", {
+
+    // Add one line to the sheet
+fetch("https://sheet.best/api/sheets/cf969697-682a-40e3-bad4-d54803eeeacf", {
     method: "POST",
     mode: "cors",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({
-        "Nombres":"Primera prueba",
-        "Telefono": "0975624601",
-        "Estado": "1"
+    body: JSON.stringify(data),
+  })
+    .then((r) => r.json())
+    .then((data) => {
+      // The response comes here
+      console.log(data);
+    })
+    .catch((error) => {
+      // Errors are reported there
+      console.log(error);
     })
    })
 })
