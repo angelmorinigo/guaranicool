4a6d5efa353ef582e57b614288ede3f93d335832
Angel Morinigo <angelmorinigo2020@gmail.com>
2023-01-11 13:04:14 -0300

    primera prueba json post

--- sheet-afumi/js/app.js
+++ sheet-afumi/js/app.js
@@ -2,5 +2,15 @@ const formulario =  document.getElementById('formulario')
 
 formulario.addEventListener('submit',(e)=>{
     e.preventDefault();
-    alert('formularios')
+   fetch('https://docs.google.com/spreadsheets/d/1BtCqGVj7cRFLuWwEcXg-UhGAxSSWoAac59sP-DFDqek/edit?usp=sharing',{
+    method: 'POST',
+    mode:'cors',
+    headers:{
+        'Content-Type':'aplication/json'
+    },
+    body: JSON.stringify({
+        "Nombres":'Primera prueba',
+        "Telefono": '0975624601'
+    })
+   })
 })
