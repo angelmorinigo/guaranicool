ebaa205c6ab1aa18065a333d87b0d2d6b3ceb9db
Angel Morinigo <angelmorinigo2020@gmail.com>
2023-01-11 16:26:05 -0300

    se elimino datos y se agrego directo

--- sheet-afumi/js/app.js
+++ sheet-afumi/js/app.js
@@ -3,27 +3,31 @@ const formulario =  document.getElementById('formulario')
    const registro  = getElementById('registro')
    const exito  = getElementById('exito')
 
-formulario.addEventListener('submit',async(e)=>{
-    e.preventDefault();
- // Add one line to the sheet
+         try{ 
+            formulario.addEventListener('submit',async(e)=>{
+                e.preventDefault();
+            // Add one line to the sheet
 
-    // Add one line to the sheet
-await fetch("https://sheet.best/api/sheets/13b8502d-4902-40aa-8fda-a4667229ee9f", {
-    method: "POST",
-    mode: "cors",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-        Nombres: formulario.nombre.value,
-        Telefono: formulario.telefono.value,
-        Estado: "1",
-        Fecha: new Date(),
-        Cedula: formulario.cedula.value,
-    }),
-  })
-    
+                // Add one line to the sheet
+            await fetch("https://sheet.best/api/sheets/13b8502d-4902-40aa-8fda-a4667229ee9f", {
+                method: "POST",
+                mode: "cors",
+                headers: {
+                "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    Nombres: formulario.nombre.value,
+                    Telefono: formulario.telefono.value,
+                    Estado: "1",
+                    Fecha: new Date(),
+                    Cedula: formulario.cedula.value,
+                }),
+            }),
+       
 
   registro.classList.remove('activo')
   exito.classList.add('activo')
    })
+}catch(e){
+    console.log(e)
+}
